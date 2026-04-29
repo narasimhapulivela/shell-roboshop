c6d4c874b49e835c0c0ccd08b86b837c12ef03c8
narasimha <narasimhapulivela@gmail.com>
2026-04-29 13:14:42 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -7,18 +7,18 @@ DOMAIN_NAME="narasimhadevops.online"
 
 for instance in $@ 
 do 
-  instance_id=$( aws ec2 run-instances \
+  INSTANCE_ID=$( aws ec2 run-instances \
   --image-id $AMI_ID \
-  --instance-type t3.micro \
+  --instance-type "t3.micro" \
   --security-group-ids $SG_ID \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
   --query 'Instances[0].InstanceId' \
   --output text )
   if [ $instance == "frontend" ]; then
-     ip=$(
+     IP=$(
       aws ec2 describe-instances \
-      --instance-ids $instance_id \
-      --query 'Reservations[0].Instances[0].PublicIpAddress' \
+      --instance-ids $INSTANCE_ID \
+      --query 'Reservations[].Instances[].PublicIpAddress' \
       --output text
      )
      RECORD_NAME="$DOMAIN_NAME"
@@ -27,7 +27,7 @@ do
      ip=$(
       aws ec2 describe-instances \
       --instance-ids $instance_id \
-      --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+      --query 'Reservations[].Instances[].PrivateIpAddress' \
       --output text
      )
      RECORD_NAME="$instance.$DOMAIN_NAME"
