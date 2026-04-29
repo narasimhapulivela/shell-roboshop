f12d2f13d92b423c7a2ba043c93df33acd116569
narasimha <narasimhapulivela@gmail.com>
2026-04-29 18:54:06 +0530

    actions

---
+++ mongodb.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
+N="/e[0m"
+
+if [ $USERID -ne 0 ]; then 
+   echo "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+if [ $1 -ne 0 ]; then 
+   echo "$2... $R FAILURE $N" | tee -a $LOGS_FILE
+   exit 1
+else
+   echo "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+fi
+
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? "Installing MongoDB server"
+
+systemctl enable mongod
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod
+VALIDATE $? "start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0.g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restart MongoDB"
